7393c232e653d4b8a7473af40ae33bec7053bcc9
MatheusMartinsR <martinsmatheusrh08@gmail.com>
2023-10-30 17:41:14 -0300

    starting TSC

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var somar = function (x, y) {
+    return x + y;
+};
+console.log("Olá tsc");
+console.log(somar(1, 2));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const somar = (x:number,y:number) => {
+    return x + y;
+};
+console.log("Olá tsc");
+console.log(somar(1,2));
